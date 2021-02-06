7714fd1bff37be2a6c3cd7dded6a5bb86a488575
Adrian Pabe <adrianpabs14@gmail.com>
2021-02-07 03:07:09 +0800

    Initial Commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [],
+
+  data() {
+    return {}
+  },
+
+  methods: {},
+}
+</script>
+
+<style scoped>
+
+</style>
